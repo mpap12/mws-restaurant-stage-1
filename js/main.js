222453084994ddb81d1e89c31684afa4c36b61b4
m.a.p <maria.papoutsi.12@gmail.com>
2019-09-28 09:14:54 +0300

    improve app accessibility and semantic HTML

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `photo of ${restaurant.name} restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.setAttribute('tabindex', '0');
   li.append(name);
